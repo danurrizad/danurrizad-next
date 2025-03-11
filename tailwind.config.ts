277041a69be1edc3e770fdf56b1a67f3e9c4802f
danurrizad <danurrizad242@gmail.com>
2025-03-11 14:57:00 +0700

    update: packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,8 @@ import type { Config } from "tailwindcss";
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -10,47 +11,112 @@ const config: Config = {
     "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      colors: {
-        'primary1': '#161A30',
-        'primary2': '#31304D',
-        'primary3': '#B6BBC4',
-        'primary4': '#F0ECE5',
-      },
-      fontFamily: {
-        'anton': ['var(--font-anton)'],
-        'garamound': ['var(--font-eb-garamound)'],
-        'neue': ['var(--font-neue)'],
-        'exo': ['var(--font-exo-2)'],
-      },
-      animation: {
-        'loop-scroll': 'loop-scroll 50s linear infinite',
-        'scrollUp': 'scrollUp 0.5s ease-in-out forwards',
-        'scrollDown': 'scrollDown 0.5s ease-in-out forwards',
-      },
-      keyframes: {
-        'loop-scroll': {
-          from: { transform: 'translateX(0)' },
-          to: { transform: 'translateX(-100%)' },
-        },
-        'scrollUp': {
-          '0%': { transform: 'translateY(100%)' },
-          '100%': { transform: 'translateY(0)' },
-        },
-        'scrollDown': {
-          '0%': { transform: 'translateY(0)' },
-          '100%': { transform: 'translateY(-100%)' },
-        },
-      }  
-    },
+  	extend: {
+  		backgroundImage: {
+  			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+  			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+  		},
+  		colors: {
+  			primary1: '#161A30',
+  			primary2: '#31304D',
+  			primary3: '#B6BBC4',
+  			primary4: '#F0ECE5',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		fontFamily: {
+  			anton: [
+  				'var(--font-anton)'
+  			],
+  			garamound: [
+  				'var(--font-eb-garamound)'
+  			],
+  			neue: [
+  				'var(--font-neue)'
+  			],
+  			exo: [
+  				'var(--font-exo-2)'
+  			]
+  		},
+  		animation: {
+  			'loop-scroll': 'loop-scroll 50s linear infinite',
+  			scrollUp: 'scrollUp 0.5s ease-in-out forwards',
+  			scrollDown: 'scrollDown 0.5s ease-in-out forwards'
+  		},
+  		keyframes: {
+  			'loop-scroll': {
+  				from: {
+  					transform: 'translateX(0)'
+  				},
+  				to: {
+  					transform: 'translateX(-100%)'
+  				}
+  			},
+  			scrollUp: {
+  				'0%': {
+  					transform: 'translateY(100%)'
+  				},
+  				'100%': {
+  					transform: 'translateY(0)'
+  				}
+  			},
+  			scrollDown: {
+  				'0%': {
+  					transform: 'translateY(0)'
+  				},
+  				'100%': {
+  					transform: 'translateY(-100%)'
+  				}
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
   plugins: [
-    require("@designbycode/tailwindcss-text-stroke")
-  ],
+    require("@designbycode/tailwindcss-text-stroke"),
+      require("tailwindcss-animate")
+],
 };
 module.exports = withMT(config);
